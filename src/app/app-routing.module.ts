af672c5f878ca6f5b06d6a056cc53303fafdef72
peterpeterparker <david.dalbusco@outlook.com>
2020-09-10 20:47:02 +0200

    chore: initial import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {Page1Component} from './page1/page1.component';
+import {Page2Component} from './page2/page2.component';
+
+const routes: Routes = [
+  { path: '', component: Page1Component },
+  { path: 'page1-component', component: Page1Component },
+  { path: 'page2-component', component: Page2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

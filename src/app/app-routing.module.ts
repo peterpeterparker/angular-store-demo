e8cfba6dc91cacdcbff9f8fad613382d93e2599e
peterpeterparker <david.dalbusco@outlook.com>
2020-09-10 20:47:54 +0200

    refactor: move components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {Page1Component} from './page1/page1.component';
-import {Page2Component} from './page2/page2.component';
+import {Page1Component} from './components/page1/page1.component';
+import {Page2Component} from './components/page2/page2.component';
 
 const routes: Routes = [
   { path: '', component: Page1Component },
